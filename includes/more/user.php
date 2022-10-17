50f762281978706e45feed4129d1f872b64b0ed8
favourloverboy <favourehio2019@gmail.com>
2022-10-17 05:16:47 +0100

    added icons on the user pages

--- includes/more/user.php
+++ includes/more/user.php
@@ -1,25 +1,25 @@
     <!-- Nav Item - Message -->
     <li class="nav-item <?php echo $active6;?>">
         <a class="nav-link" href="<?php echo $url[0], '/account';?>">
-            <i class="fas fa-user"></i>
+            <i class="fas fa-id-card"></i>
             <span>Account</span>
         </a>
     </li>
     <li class="nav-item <?php echo $active6;?>">
         <a class="nav-link" href="<?php echo $url[0], '/earnings';?>">
-            <i class="fas fa-user"></i>
+            <i class="fas fa-briefcase"></i>
             <span>Earnings</span>
         </a>
     </li>
     <li class="nav-item">
         <a class="nav-link" href="<?php echo $url[0], '/payments';?>">
-            <i class="fas fa-user"></i>
+            <i class="fas fa-credit-card"></i>
             <span>Payments</span>
         </a>
     </li>
     <li class="nav-item">
         <a class="nav-link" href="<?php echo $url[0], '/withdraws';?>">
-            <i class="fas fa-user"></i>
+            <i class="fas fa-university"></i>
             <span>Withdraws</span>
         </a>
     </li>

d3c16ca8c1fcb51af40afebdaa428e6755d32ad9
favourloverboy <favourehio2019@gmail.com>
2022-10-17 02:46:30 +0100

    updated the user account page

--- views/user/account.php
+++ views/user/account.php
@@ -1,7 +1,30 @@
 <!-- /.container-fluid -->
 <div class="container">
     <!-- Content Row -->
-    
+    <div class="row">
+        <?php 
+                
+            $tblquery = "SELECT * FROM account WHERE user_id = :id ORDER BY time DESC LIMIT 1";
+            $tblvalue = array(
+                ':id' => $_SESSION['myId']
+            );
+            $select = $connect->tbl_select($tblquery, $tblvalue);
+            if($select){
+                foreach($select as $data){
+                    extract($data);
+                    ?>
+                    <?php
+                    echo "
+                        <p>Account Name: $name ||</p>
+                        <p>Bank Name: $bank ||</p>
+                        <p>Account Number: $numbers</p>
+                    ";
+                }
+            }else{
+                echo '<p>You have not added your account yet</p>';
+            }
+        ?>
+    </div>
     <div class="row">
         <form class="form-setting" action='<?php echo $url[0], '/account'; ?>' method="POST">
             <h3>Account Details</h3>
@@ -27,27 +50,25 @@
 <?php 
     if($_POST['account']){
         extract($_POST);
-        $tblquery = "INSERT INTO account VALUES(:id, :user_id, :name, :numbers, :bank)";
+        $tblquery = "INSERT INTO account VALUES(:id, :user_id, :name, :numbers, :bank, :time)";
         $tblvalue = array(
             ':id' => NULL,
             ':user_id' => htmlspecialchars($_SESSION['myId']),
             ':name' => htmlspecialchars($name),
             ':numbers' => htmlspecialchars($number),
-            ':bank' => htmlspecialchars($bank)
+            ':bank' => htmlspecialchars($bank),
+            ':time' => time()
         );
         $insert = $connect->tbl_insert($tblquery, $tblvalue);
         if($insert){
             echo "
                 <script>
-                    swal({
-                        title: 'Updated', 
-                        text: 'Your bank details has been updated', 
-                        icon: 'success',
-                        confirmButtonColor: '#c60000'
+                    swal('Account Successful', 'Your account details has been updated, click on the button below to continue.').then(function(){
+                        window.location.href='$url[0]/account'
                     });
                 </script>
-            ";
-        }
+            "; 
+    }
     }
 
 ?>
